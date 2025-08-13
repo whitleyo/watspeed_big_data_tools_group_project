8def385bd33b9475d6ee1f1c5947bf73655eb7c2
whitleyo <owen.whitley@mail.utoronto.ca>
2025-08-13 17:39:58 -0400

    update aws startup

--- aws_startup.sh
+++ aws_startup.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # Update system packages
-sudo apt-get update -y
+sudo yum update -y
 
 # Install Docker
-sudo apt-get install -y docker.io
+sudo amazon-linux-extras enable docker
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Install Docker Compose
+# Add the 'ec2-user' to the docker group so you don't need sudo
+sudo usermod -aG docker ec2-user
+
+# Install Docker Compose (v2.x binary)
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Add the 'ubuntu' user to the docker group so you don't need sudo
-sudo usermod -aG docker ubuntu
+# Install Git
+sudo yum install -y git
 
 # Clone your repository
-# IMPORTANT: Replace the URL with your actual repository URL
-sudo apt-get install -y git
-git clone https://github.com/whitleyo/watspeed_big_data_tools_group_project.git /home/ubuntu/app
+git clone https://github.com/whitleyo/watspeed_big_data_tools_group_project.git /home/ec2-user/app
 
 # Change into the application directory and run Docker Compose
-cd /home/ubuntu/app
+cd /home/ec2-user/app
 sudo docker-compose -f docker-compose-aws.yml up -d
